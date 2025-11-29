500e441a5a5135bd4a624a3fb7e8053d7486e9ae
leo014625-cmyk <leo014625@gmail.com>
2025-11-29 14:10:52 +0100

    Update app.py

--- __project_root/app.py
+++ __project_root/app.py
@@ -9,6 +9,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from dateutil.relativedelta import relativedelta
 from collections import defaultdict
+from models import VolantinoBeta
 
 
 # ============================
